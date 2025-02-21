04127a1fbf6490030c9ce5d2f315549deecbdcbb
user1 <user1@user1ui-MacBookPro.local>
2025-02-21 14:52:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+To use this application, 
+
+1. make dev.js file inside config folder 
+2. put mongoDB info into dev.js file 
+3. Type  " npm install " inside the root directory  ( Download Server Dependencies ) 
+4. Type " npm install " inside the client directory ( Download Front-end Dependencies )
+
+
+npm install node 10.16.0
+npm install axios 
+npm install mongodb
---
+++ client/src/components/Config.js
@@ -0,0 +1,21 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+//export const API_URL = 'https://api.themoviedb.org/3/movie/550?api_key=6b194f9e4319455491c0ae3b5c8bb582';
+
+export const API_KEY = '6b194f9e4319455491c0ae3b5c8bb582';
+
+
+export  const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
